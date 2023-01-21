092d5e98e68e6ee752b83b3e90f20f14003a42fc
Алексей Рябикин <ryabikin2005@yandex.ru>
2023-01-22 02:28:59 +0300

    add working version without classes

--- temp3.py
+++
@@ -1,8 +0,0 @@
-import math
-
-# Спросим, что хорошего в этой библиотеке.
-print(print.__doc__)
-
-# Будет напечатано:
-# This module provides access to the mathematical functions
-# defined by the C standard. 
